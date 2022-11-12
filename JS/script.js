0d614f9890c2a805d90a12f003eafac05a30be17
P-Lautaro <parritalauti@gmail.com>
2022-11-12 13:50:07 -0300

    agregando elementos

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () =>{
-
-
-})
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    Map.Carousel.init(elementosCarousel,{
+        duration:160,
+        dist:-80,
+        shift:5,
+        padding:5,
+        numVisible:3,
+        indicators: true,
+        noWrap: false
+    });
+});
